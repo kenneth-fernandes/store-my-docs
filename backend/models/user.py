6044994b54d2ed9d02d8fe4834b48a68bbfc9ece
Kenneth Fernandes <kenneth.peter.fernandes@gmail.com>
2025-03-06 05:45:27 -0500

    Implemented the User class for models for querying db
    - This class serves as a Model class
    
    - Implemented methods to create and fetch users from db
    
    - Password has been hashed using bcrypt

---
+++ backend/models/user.py
@@ -0,0 +1,53 @@
+from db import connect_db
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
+
+class User:
+    def __init__(self, id, username, email, password_hash, created_at):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password_hash = password_hash
+        self.created_at = created_at
+
+
+    @staticmethod
+    def create_user(username, email, password):
+        conn = connect_db()
+        if not conn:
+            return None
+        try:
+            password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+            with conn.cursor() as cur:
+                cur.execute("INSERT INTO users (username, email, password_hash) "
+                            "VALUES (%s, %s, %s) RETURNING username;",
+                            (username, email, password_hash))
+                user_id = cur.fetchone()[0]
+            conn.commit()
+            conn.close()
+            return user_id
+        except Exception as e:
+            print("Failed to create user:", e)
+            if conn:
+                conn.close()
+            return None
+
+
+    @staticmethod
+    def find_user_by_email(email):
+        conn = connect_db()
+        if not conn:
+            return None
+
+        try:
+            with conn.cursor() as cur:
+                cur.execute("SELECT * FROM users WHERE email = %s", (email,))
+                row = cur.fetchone()
+            conn.close()
+            return User(*row) if row else None
+        except Exception as e:
+            print("Failed to fetch user:", e)
+            if conn:
+                conn.close()
+            return None
