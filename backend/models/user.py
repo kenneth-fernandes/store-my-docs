adf6a8a826d1e961d793680beab498de05a8fb17
Kenneth Fernandes <kenneth.peter.fernandes@gmail.com>
2025-03-06 22:19:03 -0500

    Implemented role based access control for User class in models
    - Implemented a function to fetch user role from the users table
    
    - Updated User class member variables by adding 'role' attribute
    
    - Updated create_user method signature to add role attribute with 'user' being the default role

--- backend/models/user.py
+++ backend/models/user.py
@@ -4,29 +4,30 @@ from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt()
 
 class User:
-    def __init__(self, id, username, email, password_hash, created_at):
+    def __init__(self, id, username, email, password_hash, role, created_at):
         self.id = id
         self.username = username
         self.email = email
         self.password_hash = password_hash
+        self.role = role
         self.created_at = created_at
 
 
     @staticmethod
-    def create_user(username, email, password):
+    def create_user(username, email, password, role="user"):
         conn = connect_db()
         if not conn:
             return None
         try:
             password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
             with conn.cursor() as cur:
-                cur.execute("INSERT INTO users (username, email, password_hash) "
-                            "VALUES (%s, %s, %s) RETURNING username;",
-                            (username, email, password_hash))
+                cur.execute("INSERT INTO users (username, email, role, password_hash) "
+                            "VALUES (%s, %s, %s, %s) RETURNING id;",
+                            (username, email, role, password_hash))
                 user_id = cur.fetchone()[0]
             conn.commit()
             conn.close()
-            return user_id
+            return user_id if user_id else None
         except Exception as e:
             print("Failed to create user:", e)
             if conn:
@@ -43,12 +44,30 @@ class User:
         try:
             with conn.cursor() as cur:
                 cur.execute("SELECT * FROM users WHERE "
-                            "username = %s OR email = %s", (identifier, identifier))
+                            "username = %s OR email = %s;", (identifier, identifier))
                 row = cur.fetchone()
             conn.close()
             return User(*row) if row else None
         except Exception as e:
             print("Failed to fetch user:", e)
+            if conn:
+                conn.close()
+            return None
+
+
+    @staticmethod
+    def get_user_role(user_id):
+        conn = connect_db()
+        if not conn:
+            return None
+        try:
+            with conn.cursor() as cur:
+                cur.execute("SELECT role FROM users WHERE id = %s;", (user_id,))
+                role = cur.fetchone()
+            conn.close()
+            return role[0] if role else None
+        except Exception as e:
+            print("Failed to fetch user's role:", e)
             if conn:
                 conn.close()
             return None
