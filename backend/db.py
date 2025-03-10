ea88098070b9f7034a62b7e7a1e34bb35070ef0a
Kenneth Fernandes <kenneth.peter.fernandes@gmail.com>
2025-03-10 06:16:15 -0400

    Implemented logging for db_connection file

--- backend/db.py
+++ backend/db.py
@@ -1,16 +1,18 @@
+import logging
+
 import psycopg2
 from config import DB_CONFIG
 
 def connect_db():
     try:
         conn = psycopg2.connect(**DB_CONFIG)
-        print("Connected to PostgreSQL!")
+        logging.info("Connected to PostgreSQL!")
         return conn
     except psycopg2.Error as e:
-        print("Database connection failed:", e)
+        logging.error(f"Database connection failed: {str(e)}")
         return None
     except Exception as e:
-        print("An error occurred:", e)
+        logging.error(f"An error occurred: {str(e)}")
         return None
 
 if __name__ == "__main__":
