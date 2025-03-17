f35b74aca995d451ba92045de39bce02727716d2
Kenneth Fernandes <kenneth.peter.fernandes@gmail.com>
2025-03-17 06:37:35 -0400

    Updated the App and Index files with the Routes and AuthContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          React App
-        </p>
-      </header>
-    </div>
+    <Router>
+      <ToastContainer position="top-center" autoClose={3000} />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
